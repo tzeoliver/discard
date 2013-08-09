a4e90f3e770d28854ebae6a4c203426273d66dca
Juha <tzeoliver@gmail.com>
2013-08-09 08:42:06 +0300

    52 cards on tabble

--- static/discard.js
+++ static/discard.js
@@ -1,6 +1,7 @@
 $(function() {
     var socket = io.connect();
 
+
     function animateFlip(card) {
         if($(card).hasClass("backfacing")){
             $(card).transition({queue: false, rotateY: "180deg"});
@@ -13,12 +14,17 @@ $(function() {
         $(this).toggleClass("backfacing");
         socket.emit("backfacing", $(this).hasClass("backfacing"));
         animateFlip(this);
+		//$(this).children.toggleClass("backface");
+	
+		console.log($(this).children());
+	
     }
 
     function createCard(suit, value) {
         var card = $('<div class="card"><div class="frontface"></div><div class="backface"></div></div>').draggable();
 
         card.find(".frontface").css("background-position", (-(value-1) * 167.538) + "px " + (-suit * 243.2) + "px");
+	
 
         card.on("drag", function(ev, ui) {
             socket.emit("move", [ui.position.top, ui.position.left]);
@@ -29,7 +35,18 @@ $(function() {
         return card;
     }
 
-    $("body").append(createCard(0, 7));
+	function createPack() {
+		var cards=0;
+		for (var i = 0; i < 4; i++){
+			for (var j=0; j < 13; j++){
+			    $("body").append(createCard(i, j));
+				cards++;
+			}
+		}
+		console.log(cards);
+	}
+
+	createPack();
 
     socket.on("move", function(position) {
         $(".card").transition({queue: false, x: position[1], y: position[0]});
